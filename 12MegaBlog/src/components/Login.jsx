8a2e83b5445aa10dccb9e712f26af7d044bd295b
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2024-07-31 04:10:13 +0000

    chai aur react

--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -37,6 +37,44 @@ function Login() {
             </span>
         </div>
         <h2 className="text-color text-2xl font-bold leading-tight">Sign in to your account</h2>
+        <p className="mt-2 text-center text-base text-black/60">
+                    Don&apos;t have any account?&nbsp;
+                    <Link
+                        to="/signup"
+                        className="font-medium text-primary transition-all duration-200 hover:underline"                    
+                    >
+                        Sign Up
+                    </Link>        
+        </p>
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+        <form onSubmit={handleSubmit(login)} className='mt-8'>
+            <div className='space-y-5'>
+                <Input 
+                label="Email: "
+                placeHolder="Enter your email"
+                type="email"
+                {...register("email", {
+                    required: true,
+                    validate: {
+                        matchPattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                        "Email address must be a valid address",
+                    }
+                })}
+                />
+                <Input 
+                label="Password: "
+                type="password"
+                placeHolder="Enter your password"
+                {...register("password", {
+                    required: true,
+                })}
+                />
+                <Button
+                type="submit"
+                className="w-full"
+                >Sign in</Button>
+            </div>
+        </form>
         </div>
     </div>
   )
